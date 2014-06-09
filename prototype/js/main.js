64585286fdfd9f2322beba28c634069293a64935
jcchen <junchangchen@gmail.com>
2014-06-10 02:08:13 +0800

    init repo

---
+++ prototype/js/main.js
@@ -0,0 +1,15 @@
+
+
+
+// background slider
+/*
+$.vegas('slideshow', {
+    delay:7000,
+    backgrounds:[
+        { src:'img/backgrounds/bg1.jpg', fade:1000 },
+        { src:'img/backgrounds/bg2.jpg', fade:1000 },
+        { src:'img/backgrounds/bg3.jpg', fade:1000 }
+    ]
+});
+ */
+
